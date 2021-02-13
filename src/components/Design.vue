16d23b4eee6237add665ce0ce0bd8b5366db0017
Febryan Ramadhan <febryanramadhan@gmail.com>
2021-02-13 19:33:06 +0700

    First commit

---
+++ src/components/Design.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="design-text-wrapper">
+        <h2 class="title-header">Best Febryan Ramadhan Design</h2>
+        <div class="design-logo-wrapper">
+            <div class="item-design">
+                <img class="design-logo" alt="Buldog logo design" src="../assets/buldog-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Dragon logo design" src="../assets/dragon-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Lion logo design" src="../assets/lion-leao-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Buldog logo design" src="../assets/buldog-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Dragon logo design" src="../assets/dragon-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Dragon logo design" src="../assets/dragon-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Lion logo design" src="../assets/lion-leao-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Buldog logo design" src="../assets/buldog-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Dragon logo design" src="../assets/dragon-logo.png">
+            </div>
+            <div class="item-design">
+                <img class="design-logo" alt="Buldog logo design" src="../assets/buldog-logo.png">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+     name: 'Design',
+}
+</script>
+
+<style scoped>
+.design-text-wrapper{
+    width: 70%;
+    overflow: hidden;
+    margin: auto;
+}
+.title-header{
+    margin-top: 80px;
+    margin-bottom: 30px;
+    text-align: left;
+}
+.design-logo-wrapper{
+    overflow: hidden;
+    margin: auto;
+    /* display: block;
+    padding-left: auto;
+    margin-right: auto; */
+    /* width: 40%; */
+}
+.item-design{
+    float: left;
+    margin: 0 10px 20px 0;
+    width: 175px;
+    height: 175px;
+    background: rgb(111, 231, 195);
+}
+
+.item-design .design-logo{
+    width: 175px;
+    height: 175px;
+}
+</style>
